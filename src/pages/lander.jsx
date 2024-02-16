f90b3af1a2640deaabf78f72ebe88e14310ab7d4
CharanMN7_pc <maverickid247@gmail.com>
2024-02-17 01:07:05 +0530

    Created necessary components and setup for page testing.
    - Created: lander.jsx, lander.css, auth.jsx, auth.css
    - added the Lander component to App.jsx for testing
    - Removed all existing boilerplate css from App.css

---
+++ src/pages/lander.jsx
@@ -0,0 +1,15 @@
+import "./lander.css";
+
+export const Lander = () => {
+  return (
+    <>
+      <main>
+        <section id="landing-text">
+          <h1>devsForFun(🚀)</h1>
+          <p>A cool place to hangout with devs from around the world!</p>
+        </section>
+        {/* Here goes the Auth stuff */}
+      </main>
+    </>
+  );
+};
